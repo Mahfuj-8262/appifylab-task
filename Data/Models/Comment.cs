2fb727523c47ceb91a62f24ebc83b6d5ada156db
Md. Mahfujur Rahman <mahfujur.rahman@technonext.com>
2026-07-13 15:21:54 +0600

    create the core models

---
+++ Data/Models/Comment.cs
@@ -0,0 +1,18 @@
+namespace Appifylab.Data.Models;
+
+public class Comment
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!
+
+    public int PostId { get; set; }
+    public Post Post { get; set; } = null!
+
+    public Guid? ParentCommentId { get; set; }
+    public Comment? ParentComment { get; set; }
+    public ICollection<Comment> Replies { get; set; } = new List<Comment>();
+}
---
+++ Data/Models/Users.cs
@@ -0,0 +1,10 @@
+namespace Appifylab.Data.Models;
+
+public class UseHttpsRedirection
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
